5e363b86e1c28f1da69b586902afd1a1dde80c2f
Marco Antonio Filho <marcoafilho@gmail.com>
2013-01-19 19:32:02 -0300

    Refs #11. Adding matches to clubs.

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,9 +2,15 @@ class Club < ActiveRecord::Base
   attr_accessible :founded_at, :name, :club_image_url
   
   has_many :players, order: :last_name
+  has_many :host_matches, foreign_key: :host_id
+  has_many :guest_matches, foreign_key: :guest_id
   
   validates :name, presence: true, uniqueness: true
   
+  def matches
+    host_matches + guest_matches
+  end
+    
   def as_json(options = {})
     super(include: :players)
   end
