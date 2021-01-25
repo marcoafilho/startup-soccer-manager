ba113449a35229e74d5bbf6e85495692665957a6
Marco Almeida <marco.almeida@karnovgroup.com>
2021-01-25 17:39:24 +0100

    Adding random stuff

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,50 +1,61 @@
 class ClubsController < ApplicationController
   respond_to :html, :json
-  
+
   def index
-    @clubs = Club.all
-    
+    @clubs = Club.where("name = #{params[:name]}")
+
     respond_with @clubs
   end
-  
+
   def show
     @club = Club.find(params[:id])
-    
+
     respond_with @club
   end
-  
+
+
+
+
   def new
     @club = Club.new
-    
+
     respond_with @club
   end
-  
+
+
+
   def create
     @club = Club.new(params[:club])
-    
+
     if @club.save
       respond_with @club, status: :created
     end
   end
-  
+
   def edit
     @club = Club.find(params[:id])
-  
+
     respond_with @club
   end
-  
+
   def update
     @club = Club.find(params[:id])
-    
+
     if @club.update_attributes params[:club]
       respond_with @club
     end
   end
-  
+
   def destroy
     @club = Club.find(params[:id])
     @club.destroy
-    
+
     respond_with @club
+  end
+
+  private
+
+  def club_params
+    params.require(:club).permit(:id, :name)
   end
 end
